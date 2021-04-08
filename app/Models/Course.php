6b187981e132f33cf9dca6d7fb1861610182448b
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-04-08 15:33:05 -0300

    feat (model): adding the course model

---
+++ app/Models/Course.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'courses';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_course';
+
+    protected $fillable = [
+        'name',
+        'start_course',
+        'end_course',
+        'period'
+    ];
+}
