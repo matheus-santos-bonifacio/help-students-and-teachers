c558f88623e5ec02418cc804caf0b7750f33fde0
Matheus dos Santos Bonifácio <caneladeouro10@gmail.com>
2021-04-08 15:19:43 -0300

    feat (view): change the register course page

--- resources/views/register-course.blade.php
+++ resources/views/register-course.blade.php
@@ -8,19 +8,29 @@
         <form method="post" action="/register-course" class="row g-3">
             @csrf
 
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <label for="validationName" class="form-label">Name</label>
                 <input type="text" name="name" class="form-control" id="validationName" required>
             </div>
 
-            <div class="col-md-4">
-                <label for="validationStartTime" class="form-label">Start time</label>
-                <input type="text" name="age" class="form-control" id="validationStartTime" placeholder="xx/xx/xxxx - xx/xx/xxxx" required>
+            <div class="col-md-6">
+                <label for="validationEndTime" class="form-label">Period</label>
+                <select name="email" class="form-select" aria-label="Default select example" required>
+                    <option selected>Open the period</option>
+                    <option value="Morning">Morning</option>
+                    <option value="Evening">Evening</option>
+                    <option value="Night">Night</option>
+                </select>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-6">
+                <label for="validationStartTime" class="form-label">Start time</label>
+                <input type="text" name="age" class="form-control" id="validationStartTime" placeholder="xx/xx/xxxx" required>
+            </div>
+
+            <div class="col-md-6">
                 <label for="validationEndTime" class="form-label">End time</label>
-                <input type="text" name="email" class="form-control" id="validationEndTime" placeholder="xx/xx/xxxx - xx/xx/xxxx" required>
+                <input type="text" name="email" class="form-control" id="validationEndTime" placeholder="xx/xx/xxxx" required>
             </div>
 
             <div class="col-12">
